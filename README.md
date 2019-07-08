8c5f4ac6f60f232ba9655faba44799922679f49d
omdriven <indie@yandex.ru>
2019-07-08 20:20:35 +0200

    create new layout

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Official editor is great, but when it comes to hundreds of controls a little hack might be sufficient. Here's the script which converts .touchosc presets to .zip archives and derives its XML data.
+Official editor is great, but when it comes to hundreds of controls a little hack can be sufficient. Here's the script which converts .touchosc presets to .zip archives and derives its XML data.
 
 Also it's supposed to work only for my specific layouts, so don't expect too much from it, but just inspiration to make it better yourself.
 
--- decode.js
+++ decode.js
@@ -13,18 +13,4 @@ const replace = src_str.replace(/name=\"([A-Za-z0-9+/=]*)\"*/gi, (match, offset,
 
 console.log(replace);
 
-// fs.writeFile('index' + Date.now() + '.xml', replace);
-fs.writeFile('result.xml', replace);
-// let result = '';
-
-// name.forEach((str) => {
-// 	const val = str.split('"')[1];
-// 	var buf = new Buffer(val, 'base64'); // Ta-da
-// 	const buf_str = String(buf);
-
-// 	console.log(String(buf), val);
-// 	result = src_str.replace(val, buf_str);
-// })
-
-// console.log(result);
-// console.log(name.length);
+// fs.writeFile('result.xml', replace);
